419848307f5b9eb978961ed9623e0e31900b4737
BE NULL <benull@BEdeMacBook-Pro.local>
2020-07-21 10:17:38 +0800

    [141] Linked List Cycle

---
+++ 141.linked-list-cycle.py
@@ -0,0 +1,91 @@
+#
+# @lc app=leetcode id=141 lang=python3
+#
+# [141] Linked List Cycle
+#
+# https://leetcode.com/problems/linked-list-cycle/description/
+#
+# algorithms
+# Easy (40.95%)
+# Likes:    2926
+# Dislikes: 491
+# Total Accepted:    647.1K
+# Total Submissions: 1.6M
+# Testcase Example:  '[3,2,0,-4]\n1'
+#
+# Given a linked list, determine if it has a cycle in it.
+# 
+# To represent a cycle in the given linked list, we use an integer pos which
+# represents the position (0-indexed) in the linked list where tail connects
+# to. If pos is -1, then there is no cycle in the linked list.
+# 
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: head = [3,2,0,-4], pos = 1
+# Output: true
+# Explanation: There is a cycle in the linked list, where tail connects to the
+# second node.
+# 
+# 
+# 
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: head = [1,2], pos = 0
+# Output: true
+# Explanation: There is a cycle in the linked list, where tail connects to the
+# first node.
+# 
+# 
+# 
+# 
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: head = [1], pos = -1
+# Output: false
+# Explanation: There is no cycle in the linked list.
+# 
+# 
+# 
+# 
+# 
+# 
+# 
+# Follow up:
+# 
+# Can you solve it using O(1) (i.e. constant) memory?
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # Two Pointer
+    def hasCycle(self, head: ListNode) -> bool:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            if fast.next == slow:
+                return True
+            fast = fast.next.next
+            slow = slow.next
+        return False
+
+        
+# @lc code=end
+
