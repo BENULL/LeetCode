7de16d1845f92f4e9afe249c6a38473ae784f1d6
BE NULL <327137362@qq.com>
2020-07-28 21:16:26 +0800

    [142] Linked List Cycle II

---
+++ 142.linked-list-cycle-ii.py
@@ -0,0 +1,92 @@
+#
+# @lc app=leetcode id=142 lang=python3
+#
+# [142] Linked List Cycle II
+#
+# https://leetcode.com/problems/linked-list-cycle-ii/description/
+#
+# algorithms
+# Medium (37.03%)
+# Likes:    2795
+# Dislikes: 223
+# Total Accepted:    329.9K
+# Total Submissions: 888.7K
+# Testcase Example:  '[3,2,0,-4]\n1'
+#
+# Given a linked list, return the node where the cycle begins. If there is no
+# cycle, return null.
+# 
+# To represent a cycle in the given linked list, we use an integer pos which
+# represents the position (0-indexed) in the linked list where tail connects
+# to. If pos is -1, then there is no cycle in the linked list.
+# 
+# Note: Do not modify the linked list.
+# 
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: head = [3,2,0,-4], pos = 1
+# Output: tail connects to node index 1
+# Explanation: There is a cycle in the linked list, where tail connects to the
+# second node.
+# 
+# 
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: head = [1,2], pos = 0
+# Output: tail connects to node index 0
+# Explanation: There is a cycle in the linked list, where tail connects to the
+# first node.
+# 
+# 
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: head = [1], pos = -1
+# Output: no cycle
+# Explanation: There is no cycle in the linked list.
+# 
+# 
+# 
+# 
+# 
+# 
+# Follow-up:
+# Can you solve it without using extra space?
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    # two pointer
+    # 2(H+D) = H + nC + D
+    # H = nC -D
+    def detectCycle(self, head: ListNode) -> ListNode:
+        try:
+            slow = head.next
+            fast = head.next.next
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next.next
+        except:
+            return None
+        slow = head
+        while slow != fast:
+            slow = slow.next
+            fast = fast.next
+        return slow
+# @lc code=end
+
