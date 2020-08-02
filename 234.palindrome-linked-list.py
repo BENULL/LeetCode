699e31ae7a46786da1264bedd64b13f6547426fe
BE NULL <327137362@qq.com>
2020-08-02 18:39:41 +0800

    [234] Palindrome Linked List

---
+++ 234.palindrome-linked-list.py
@@ -0,0 +1,59 @@
+#
+# @lc app=leetcode id=234 lang=python3
+#
+# [234] Palindrome Linked List
+#
+# https://leetcode.com/problems/palindrome-linked-list/description/
+#
+# algorithms
+# Easy (39.09%)
+# Likes:    3302
+# Dislikes: 365
+# Total Accepted:    438.3K
+# Total Submissions: 1.1M
+# Testcase Example:  '[1,2]'
+#
+# Given a singly linked list, determine if it is a palindrome.
+# 
+# Example 1:
+# 
+# 
+# Input: 1->2
+# Output: false
+# 
+# Example 2:
+# 
+# 
+# Input: 1->2->2->1
+# Output: true
+# 
+# Follow up:
+# Could you do it in O(n) time and O(1) space?
+# 
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    # Phase 1: Reverse the first half while finding the middle.
+    # Phase 2: Compare the reversed first half with the second half.
+    def isPalindrome(self, head: ListNode) -> bool:
+        rev = None
+        slow = fast = head
+        while fast and fast.next:
+            fast = fast.next.next
+            rev, rev.next, slow = slow, rev, slow.next  
+        if fast:
+            slow = slow.next
+        while rev and rev.val == slow.val:
+            slow = slow.next
+            rev = rev.next
+        return not rev
+        
+        
+# @lc code=end
+
