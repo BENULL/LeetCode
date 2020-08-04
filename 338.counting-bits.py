c67c564681c61cd69f7a77e5b5fe5d5415b83c37
BE NULL <327137362@qq.com>
2020-08-04 15:41:40 +0800

    [338] Counting Bits

---
+++ 338.counting-bits.py
@@ -0,0 +1,54 @@
+#
+# @lc app=leetcode id=338 lang=python3
+#
+# [338] Counting Bits
+#
+# https://leetcode.com/problems/counting-bits/description/
+#
+# algorithms
+# Medium (69.39%)
+# Likes:    2790
+# Dislikes: 171
+# Total Accepted:    292.6K
+# Total Submissions: 421.1K
+# Testcase Example:  '2'
+#
+# Given a non negative integer number num. For every numbers i in the range 0 ≤
+# i ≤ num calculate the number of 1's in their binary representation and return
+# them as an array.
+# 
+# Example 1:
+# 
+# 
+# Input: 2
+# Output: [0,1,1]
+# 
+# Example 2:
+# 
+# 
+# Input: 5
+# Output: [0,1,1,2,1,2]
+# 
+# 
+# Follow up:
+# 
+# 
+# It is very easy to come up with a solution with run time
+# O(n*sizeof(integer)). But can you do it in linear time O(n) /possibly in a
+# single pass?
+# Space complexity should be O(n).
+# Can you do it like a boss? Do it without using any builtin function like
+# __builtin_popcount in c++ or in any other language.
+# 
+#
+
+# @lc code=start
+class Solution:
+    def countBits(self, num: int) -> List[int]:
+        res = [0]*(num+1)
+        for num_ in range(num+1):
+            res[num_] = res[ num_ // 2 ] if num_ % 2 == 0 else res[num_ // 2 ] + 1
+        return res
+        
+# @lc code=end
+
