4c671e0c139765d303d28b0a15edeceff9fc1c19
BE NULL <327137362@qq.com>
2020-08-31 17:33:06 +0800

    [107] Binary Tree Level Order Traversal II

---
+++ 107.binary-tree-level-order-traversal-ii.py
@@ -0,0 +1,59 @@
+#
+# @lc app=leetcode id=107 lang=python3
+#
+# [107] Binary Tree Level Order Traversal II
+#
+# https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+#
+# algorithms
+# Easy (53.28%)
+# Likes:    1603
+# Dislikes: 217
+# Total Accepted:    366.2K
+# Total Submissions: 681.4K
+# Testcase Example:  '[3,9,20,null,null,15,7]'
+#
+# Given a binary tree, return the bottom-up level order traversal of its nodes'
+# values. (ie, from left to right, level by level from leaf to root).
+# 
+# 
+# For example:
+# Given binary tree [3,9,20,null,null,15,7],
+# 
+# ⁠   3
+# ⁠  / \
+# ⁠ 9  20
+# ⁠   /  \
+# ⁠  15   7
+# 
+# 
+# 
+# return its bottom-up level order traversal as:
+# 
+# [
+# ⁠ [15,7],
+# ⁠ [9,20],
+# ⁠ [3]
+# ]
+# 
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrderBottom(self, root: TreeNode) -> List[List[int]]:
+        res, q = [],[root]
+        while root and q:
+            res.insert(0,[node.val for node in q])
+            q = [child for node in q for child in (node.left,node.right) if child]
+        return res
+
+        
+# @lc code=end
+
