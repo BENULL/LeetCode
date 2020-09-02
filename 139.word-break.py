72de83f1130f0058ba6f9e124f4c39814d2290c2
BE NULL <327137362@qq.com>
2020-09-02 10:33:55 +0800

    [139] Word Break

---
+++ 139.word-break.py
@@ -0,0 +1,69 @@
+#
+# @lc app=leetcode id=139 lang=python3
+#
+# [139] Word Break
+#
+# https://leetcode.com/problems/word-break/description/
+#
+# algorithms
+# Medium (39.82%)
+# Likes:    4387
+# Dislikes: 233
+# Total Accepted:    559K
+# Total Submissions: 1.4M
+# Testcase Example:  '"leetcode"\n["leet","code"]'
+#
+# Given a non-empty string s and a dictionary wordDict containing a list of
+# non-empty words, determine if s can be segmented into a space-separated
+# sequence of one or more dictionary words.
+# 
+# Note:
+# 
+# 
+# The same word in the dictionary may be reused multiple times in the
+# segmentation.
+# You may assume the dictionary does not contain duplicate words.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: s = "leetcode", wordDict = ["leet", "code"]
+# Output: true
+# Explanation: Return true because "leetcode" can be segmented as "leet
+# code".
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: s = "applepenapple", wordDict = ["apple", "pen"]
+# Output: true
+# Explanation: Return true because "applepenapple" can be segmented as "apple
+# pen apple".
+# Note that you are allowed to reuse a dictionary word.
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+# Output: false
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    # dp
+    def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        dp = [False]*(len(s)+1) 
+        dp[0] = True
+        for i in range(len(s)):
+            for j in range(i,len(s)):
+                if dp[i] and s[i:j+1] in wordDict:
+                    dp[j+1] = True
+        return dp[-1]
+        
+# @lc code=end
+
