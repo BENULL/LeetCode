f70970a0037c7b05923bffa3674b7746d6f2ccfd
BENULL <327137362@qq.com>
2020-11-26 11:17:04 +0800

    [129] Sum Root to Leaf Numbers

---
+++ 129.sum-root-to-leaf-numbers.py
@@ -0,0 +1,83 @@
+#
+# @lc app=leetcode id=129 lang=python3
+#
+# [129] Sum Root to Leaf Numbers
+#
+# https://leetcode.com/problems/sum-root-to-leaf-numbers/description/
+#
+# algorithms
+# Medium (48.78%)
+# Likes:    1869
+# Dislikes: 45
+# Total Accepted:    301.8K
+# Total Submissions: 602K
+# Testcase Example:  '[1,2,3]'
+#
+# Given a binary tree containing digits from 0-9 only, each root-to-leaf path
+# could represent a number.
+# 
+# An example is the root-to-leaf path 1->2->3 which represents the number 123.
+# 
+# Find the total sum of all root-to-leaf numbers.
+# 
+# Note: A leaf is a node with no children.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,3]
+# ⁠   1
+# ⁠  / \
+# ⁠ 2   3
+# Output: 25
+# Explanation:
+# The root-to-leaf path 1->2 represents the number 12.
+# The root-to-leaf path 1->3 represents the number 13.
+# Therefore, sum = 12 + 13 = 25.
+# 
+# Example 2:
+# 
+# 
+# Input: [4,9,0,5,1]
+# ⁠   4
+# ⁠  / \
+# ⁠ 9   0
+# / \
+# 5   1
+# Output: 1026
+# Explanation:
+# The root-to-leaf path 4->9->5 represents the number 495.
+# The root-to-leaf path 4->9->1 represents the number 491.
+# The root-to-leaf path 4->0 represents the number 40.
+# Therefore, sum = 495 + 491 + 40 = 1026.
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:
+        def dfs(root,_sum):
+            if not root:
+                return 0
+            if not root.left and not root.right:
+                return _sum*10 + root.val
+            left, right=0, 0 
+            if root.left:
+                left = dfs(root.left,_sum*10+root.val)
+            if root.right:
+                right = dfs(root.right,_sum*10+root.val)
+            return left+right
+        return dfs(root,0)
+
+            
+
+
+        
+# @lc code=end
+
