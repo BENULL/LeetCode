bb1d8ae52388352b701d3340a48d718798fc6c36
BENULL <327137362@qq.com>
2020-11-28 09:40:09 +0800

    [199] Binary Tree Right Side View

---
+++ 199.binary-tree-right-side-view.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode id=199 lang=python3
+#
+# [199] Binary Tree Right Side View
+#
+# https://leetcode.com/problems/binary-tree-right-side-view/description/
+#
+# algorithms
+# Medium (53.81%)
+# Likes:    2981
+# Dislikes: 167
+# Total Accepted:    354K
+# Total Submissions: 640.6K
+# Testcase Example:  '[1,2,3,null,5,null,4]'
+#
+# Given a binary tree, imagine yourself standing on the right side of it,
+# return the values of the nodes you can see ordered from top to bottom.
+# 
+# Example:
+# 
+# 
+# Input: [1,2,3,null,5,null,4]
+# Output: [1, 3, 4]
+# Explanation:
+# 
+# ⁠  1            <---
+# ⁠/   \
+# 2     3         <---
+# ⁠\     \
+# ⁠ 5     4       <---
+# 
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: TreeNode) -> List[int]:
+        # bfs
+        # res, q = [],[root]
+        # while root and q:
+        #     res.append(q[-1].val)
+        #     q = [child for node in q for child in (node.left,node.right) if child]
+        # return res
+
+        # dfs
+        def dfs(root,level):
+            if not root:
+                return None
+            if len(res)==level:
+                res.append(root.val)
+            dfs(root.right,level+1)
+            dfs(root.left,level+1)
+        
+        res = []
+        dfs(root,0)
+        return res
+        
+
+
+        
+# @lc code=end
+
