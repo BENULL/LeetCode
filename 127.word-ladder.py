65d1ce002b3b586f80c04399a30148b1c3733d15
BENULL <327137362@qq.com>
2020-12-26 15:41:34 +0800

    [127] Word Ladder

---
+++ 127.word-ladder.py
@@ -0,0 +1,100 @@
+#
+# @lc app=leetcode id=127 lang=python3
+#
+# [127] Word Ladder
+#
+# https://leetcode.com/problems/word-ladder/description/
+#
+# algorithms
+# Medium (29.29%)
+# Likes:    3228
+# Dislikes: 1175
+# Total Accepted:    433.4K
+# Total Submissions: 1.5M
+# Testcase Example:  '"hit"\n"cog"\n["hot","dot","dog","lot","log","cog"]'
+#
+# Given two words (beginWord and endWord), and a dictionary's word list, find
+# the length of shortest transformation sequence from beginWord to endWord,
+# such that:
+# 
+# 
+# Only one letter can be changed at a time.
+# Each transformed word must exist in the word list.
+# 
+# 
+# Note:
+# 
+# 
+# Return 0 if there is no such transformation sequence.
+# All words have the same length.
+# All words contain only lowercase alphabetic characters.
+# You may assume no duplicates in the word list.
+# You may assume beginWord and endWord are non-empty and are not the same.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input:
+# beginWord = "hit",
+# endWord = "cog",
+# wordList = ["hot","dot","dog","lot","log","cog"]
+# 
+# Output: 5
+# 
+# Explanation: As one shortest transformation is "hit" -> "hot" -> "dot" ->
+# "dog" -> "cog",
+# return its length 5.
+# 
+# 
+# Example 2:
+# 
+# 
+# Input:
+# beginWord = "hit"
+# endWord = "cog"
+# wordList = ["hot","dot","dog","lot","log"]
+# 
+# Output: 0
+# 
+# Explanation: The endWord "cog" is not in wordList, therefore no possible
+# transformation.
+# 
+# 
+# 
+# 
+# 
+#
+
+# @lc code=start
+
+
+class Solution:
+    def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        if endWord not in wordList or not endWord or not beginWord or not wordList:
+            return 0
+        L = len(beginWord)
+        all_combo_dict = defaultdict(list)
+        for word in wordList:
+            for i in range(L):
+                all_combo_dict[word[:i] + "*" + word[i+1:]].append(word) 
+        queue = deque([(beginWord, 1)])
+        visited = set()
+        visited.add(beginWord)
+        while queue:
+            current_word, level = queue.popleft()
+            for i in range(L):
+                intermediate_word = current_word[:i] + "*" + current_word[i+1:]
+                for word in all_combo_dict[intermediate_word]:
+                    if word == endWord:
+                        return level + 1
+                    if word not in visited:
+                        visited.add(word)
+                        queue.append((word, level + 1))
+        return 0
+        
+
+
+        
+# @lc code=end
+
