72172ea3e40ee68e31209f01fdd2af24f03c2dac
BENULL <327137362@qq.com>
2021-01-05 11:15:15 +0800

    [150] Evaluate Reverse Polish Notation

---
+++ 150.evaluate-reverse-polish-notation.py
@@ -0,0 +1,89 @@
+#
+# @lc app=leetcode id=150 lang=python3
+#
+# [150] Evaluate Reverse Polish Notation
+#
+# https://leetcode.com/problems/evaluate-reverse-polish-notation/description/
+#
+# algorithms
+# Medium (36.12%)
+# Likes:    1353
+# Dislikes: 500
+# Total Accepted:    260K
+# Total Submissions: 693.7K
+# Testcase Example:  '["2","1","+","3","*"]'
+#
+# Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+# 
+# Valid operators are +, -, *, /. Each operand may be an integer or another
+# expression.
+# 
+# Note:
+# 
+# 
+# Division between two integers should truncate toward zero.
+# The given RPN expression is always valid. That means the expression would
+# always evaluate to a result and there won't be any divide by zero
+# operation.
+# 
+# 
+# Example 1:
+# 
+# 
+# Input: ["2", "1", "+", "3", "*"]
+# Output: 9
+# Explanation: ((2 + 1) * 3) = 9
+# 
+# 
+# Example 2:
+# 
+# 
+# Input: ["4", "13", "5", "/", "+"]
+# Output: 6
+# Explanation: (4 + (13 / 5)) = 6
+# 
+# 
+# Example 3:
+# 
+# 
+# Input: ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
+# Output: 22
+# Explanation: 
+# ⁠ ((10 * (6 / ((9 + 3) * -11))) + 17) + 5
+# = ((10 * (6 / (12 * -11))) + 17) + 5
+# = ((10 * (6 / -132)) + 17) + 5
+# = ((10 * 0) + 17) + 5
+# = (0 + 17) + 5
+# = 17 + 5
+# = 22
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def evalRPN(self, tokens: List[str]) -> int:
+        stack = []
+        for s in tokens:
+            if s not in "+-*/":
+                stack.append(int(s))
+            else:
+                right = stack.pop()
+                left = stack.pop()
+                if s == '+':
+                    stack.append(left+right)
+                elif s == '-':
+                    stack.append(left-right)
+                elif s == '*':
+                    stack.append(left*right)
+                else:
+                    stack.append(int(left/right))
+                
+                
+        return stack.pop()
+
+
+
+        
+# @lc code=end
+
