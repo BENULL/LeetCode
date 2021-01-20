27c44dd9082ddc4f747bc87a472ede58f7fca366
BENULL <327137362@qq.com>
2021-01-20 11:08:46 +0800

    [441] Arranging Coins

---
+++ 441.arranging-coins.py
@@ -0,0 +1,68 @@
+#
+# @lc app=leetcode id=441 lang=python3
+#
+# [441] Arranging Coins
+#
+# https://leetcode.com/problems/arranging-coins/description/
+#
+# algorithms
+# Easy (41.90%)
+# Likes:    807
+# Dislikes: 764
+# Total Accepted:    180.9K
+# Total Submissions: 427.6K
+# Testcase Example:  '5'
+#
+# You have a total of n coins that you want to form in a staircase shape, where
+# every k-th row must have exactly k coins.
+# ⁠
+# Given n, find the total number of full staircase rows that can be formed.
+# 
+# n is a non-negative integer and fits within the range of a 32-bit signed
+# integer.
+# 
+# Example 1:
+# 
+# n = 5
+# 
+# The coins can form the following rows:
+# ¤
+# ¤ ¤
+# ¤ ¤
+# 
+# Because the 3rd row is incomplete, we return 2.
+# 
+# 
+# 
+# Example 2:
+# 
+# n = 8
+# 
+# The coins can form the following rows:
+# ¤
+# ¤ ¤
+# ¤ ¤ ¤
+# ¤ ¤
+# 
+# Because the 4th row is incomplete, we return 3.
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+        left, right = 0, n
+        while left <= right:
+            k = (right + left) // 2
+            curr = k * (k + 1) // 2
+            if curr == n:
+                return k
+            if n < curr:
+                right = k - 1
+            else:
+                left = k + 1
+        return right
+        
+# @lc code=end
+
