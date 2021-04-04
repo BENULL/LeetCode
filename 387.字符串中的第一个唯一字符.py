b8daadf16a7b9137df93d303dcbe51c87a5cf58d
BENULL <327137362@qq.com>
2021-04-04 21:26:22 +0800

    [387] 字符串中的第一个唯一字符

---
+++ 387.字符串中的第一个唯一字符.py
@@ -0,0 +1,45 @@
+#
+# @lc app=leetcode.cn id=387 lang=python3
+#
+# [387] 字符串中的第一个唯一字符
+#
+# https://leetcode-cn.com/problems/first-unique-character-in-a-string/description/
+#
+# algorithms
+# Easy (51.65%)
+# Likes:    366
+# Dislikes: 0
+# Total Accepted:    170.6K
+# Total Submissions: 330.3K
+# Testcase Example:  '"leetcode"'
+#
+# 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+# 
+# 
+# 
+# 示例：
+# 
+# s = "leetcode"
+# 返回 0
+# 
+# s = "loveleetcode"
+# 返回 2
+# 
+# 
+# 
+# 
+# 提示：你可以假定该字符串只包含小写字母。
+# 
+#
+
+# @lc code=start
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        frequency = collections.Counter(s)
+        for i, ch in enumerate(s):
+            if frequency[ch] == 1:
+                return i
+        return -1
+
+# @lc code=end
+
