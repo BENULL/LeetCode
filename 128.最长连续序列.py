9fb51bb2e91b88655fef970faae3efc23f95c9d4
BENULL <327137362@qq.com>
2021-06-10 21:02:19 +0800

    hash

---
+++ 128.最长连续序列.py
@@ -0,0 +1,69 @@
+#
+# @lc app=leetcode.cn id=128 lang=python3
+#
+# [128] 最长连续序列
+#
+# https://leetcode-cn.com/problems/longest-consecutive-sequence/description/
+#
+# algorithms
+# Medium (53.83%)
+# Likes:    799
+# Dislikes: 0
+# Total Accepted:    127.8K
+# Total Submissions: 237.5K
+# Testcase Example:  '[100,4,200,1,3,2]'
+#
+# 给定一个未排序的整数数组 nums ，找出数字连续的最长序列（不要求序列元素在原数组中连续）的长度。
+# 
+# 
+# 
+# 进阶：你可以设计并实现时间复杂度为 O(n) 的解决方案吗？
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：nums = [100,4,200,1,3,2]
+# 输出：4
+# 解释：最长数字连续序列是 [1, 2, 3, 4]。它的长度为 4。
+# 
+# 示例 2：
+# 
+# 
+# 输入：nums = [0,3,7,2,5,8,4,6,0,1]
+# 输出：9
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 0 
+# -10^9 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        longest_streak = 0
+        num_set = set(nums)
+
+        for num in num_set:
+            if num - 1 not in num_set:
+                current_num = num
+                current_streak = 1
+
+                while current_num + 1 in num_set:
+                    current_num += 1
+                    current_streak += 1
+
+                longest_streak = max(longest_streak, current_streak)
+
+        return longest_streak
+
+# @lc code=end
+
