ee6b89cc9f53c17bb84845ba7ff662ad021f17fc
BENULL <327137362@qq.com>
2022-01-03 16:12:37 +0800

    catalan num

---
+++ 96.不同的二叉搜索树.py
@@ -0,0 +1,67 @@
+#
+# @lc app=leetcode.cn id=96 lang=python3
+#
+# [96] 不同的二叉搜索树
+#
+# https://leetcode-cn.com/problems/unique-binary-search-trees/description/
+#
+# algorithms
+# Medium (69.95%)
+# Likes:    1468
+# Dislikes: 0
+# Total Accepted:    186.9K
+# Total Submissions: 267.1K
+# Testcase Example:  '3'
+#
+# 给你一个整数 n ，求恰由 n 个节点组成且节点值从 1 到 n 互不相同的 二叉搜索树 有多少种？返回满足题意的二叉搜索树的种数。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：n = 3
+# 输出：5
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：n = 1
+# 输出：1
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def numTrees(self, n: int) -> int:
+        # math catalan
+
+        # C = 1
+        # for i in range(0, n):
+        #     C = C * 2*(2*i+1)/(i+2)
+        # return int(C)
+
+        # dp 以i为根 f = g(i-1)*g(n-i)
+        G = [0]*(n+1)
+        G[0], G[1] = 1, 1
+
+        for i in range(2, n+1):
+            for j in range(1, i+1):
+                G[i] += G[j-1] * G[i-j]
+
+        return G[n]
+
+
+
+# @lc code=end
+
