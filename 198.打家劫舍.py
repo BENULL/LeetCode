e7dbefef54fd11aff838bd235bc8fe472a4e2175
BENULL <327137362@qq.com>
2022-02-15 15:58:57 +0800

    ***dp 打家劫舍

--- 198.打家劫舍.py
+++ 198.打家劫舍.py
@@ -58,13 +58,21 @@ class Solution:
         if size == 1:
             return nums[0]
         
-        dp = [0] * size
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, size):
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        # dp = [0] * size
+        # dp[0] = nums[0]
+        # dp[1] = max(nums[0], nums[1])
+        # for i in range(2, size):
+        #     dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
         
-        return dp[size - 1]
+        # return dp[size - 1]
+
+        # 滚动
+        first = nums[0]
+        second = max(nums[0], nums[1])
+        for i in range(2,len(nums)):
+            first, second = second, max(first+nums[i], second)
+        return second
+
 
 # @lc code=end
 
---
+++ 213.打家劫舍-ii.py
@@ -0,0 +1,75 @@
+#
+# @lc app=leetcode.cn id=213 lang=python3
+#
+# [213] 打家劫舍 II
+#
+# https://leetcode-cn.com/problems/house-robber-ii/description/
+#
+# algorithms
+# Medium (42.88%)
+# Likes:    732
+# Dislikes: 0
+# Total Accepted:    141.6K
+# Total Submissions: 329.1K
+# Testcase Example:  '[2,3,2]'
+#
+# 你是一个专业的小偷，计划偷窃沿街的房屋，每间房内都藏有一定的现金。这个地方所有的房屋都 围成一圈
+# ，这意味着第一个房屋和最后一个房屋是紧挨着的。同时，相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警 。
+# 
+# 给定一个代表每个房屋存放金额的非负整数数组，计算你 在不触动警报装置的情况下 ，今晚能够偷窃到的最高金额。
+# 
+# 
+# 
+# 示例 1：
+# 
+# 
+# 输入：nums = [2,3,2]
+# 输出：3
+# 解释：你不能先偷窃 1 号房屋（金额 = 2），然后偷窃 3 号房屋（金额 = 2）, 因为他们是相邻的。
+# 
+# 
+# 示例 2：
+# 
+# 
+# 输入：nums = [1,2,3,1]
+# 输出：4
+# 解释：你可以先偷窃 1 号房屋（金额 = 1），然后偷窃 3 号房屋（金额 = 3）。
+# 偷窃到的最高金额 = 1 + 3 = 4 。
+# 
+# 示例 3：
+# 
+# 
+# 输入：nums = [0]
+# 输出：0
+# 
+# 
+# 
+# 
+# 提示：
+# 
+# 
+# 1 
+# 0 
+# 
+# 
+#
+
+# @lc code=start
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def robRange(start: int, end: int) -> int:
+            first = nums[start]
+            second = max(nums[start], nums[start + 1])
+            for i in range(start + 2, end + 1):
+                first, second = second, max(first + nums[i], second)
+            return second
+        
+        length = len(nums)
+        if length == 1:
+            return nums[0]
+        elif length == 2:
+            return max(nums[0], nums[1])
+        else:
+            return max(robRange(0, length - 2), robRange(1, length - 1))
+# @lc code=end
+
