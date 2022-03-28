21559d4aded7c1efd5ff34b38b66ec06848da0d3
BENULL <327137362@qq.com>
2022-03-28 16:50:28 +0800

    topk

---
+++ Offer 40. 最小的k个数.py
@@ -0,0 +1,41 @@
+class Solution:
+    """
+    输入整数数组 arr ，找出其中最小的 k 个数。例如，输入4、5、1、6、2、7、3、8这8个数字，则最小的4个数字是1、2、3、4。
+    示例 1：
+
+
+    输入：arr = [3,2,1], k = 2
+    输出：[1,2] 或者 [2,1]
+    示例 2：
+
+    输入：arr = [0,1,2,1], k = 1
+    输出：[0]
+     
+
+    限制：
+
+    0 <= k <= arr.length <= 10000
+    0 <= arr[i] <= 10000
+
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/zui-xiao-de-kge-shu-lcof
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    """
+    def getLeastNumbers(self, arr: List[int], k: int) -> List[int]:
+        # return sorted(arr)[:k]
+        if k >= len(arr): return arr
+        def quick_sort(l, r):
+            i, j = l, r
+            while i < j:
+                while i<j and arr[j]>=arr[l]:
+                    j -= 1
+                while i<j and arr[i]<=arr[l]:
+                    i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+            arr[l], arr[i] = arr[i], arr[l]
+            if k < i: return quick_sort(l, i - 1) 
+            if k > i: return quick_sort(i + 1, r)
+            return arr[:k]
+        
+        return quick_sort(0, len(arr)-1)
