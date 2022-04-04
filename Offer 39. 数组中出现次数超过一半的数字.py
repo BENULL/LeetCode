85d14809c3d25aaf885297ac59323c464354d894
BENULL <327137362@qq.com>
2022-04-04 12:46:29 +0800

    众数

---
+++ Offer 39. 数组中出现次数超过一半的数字.py
@@ -0,0 +1,41 @@
+class Solution:
+    """
+    数组中有一个数字出现的次数超过数组长度的一半，请找出这个数字。
+
+ 
+
+    你可以假设数组是非空的，并且给定的数组总是存在多数元素。
+
+     
+
+    示例 1:
+
+    输入: [1, 2, 3, 2, 2, 2, 5, 4, 2]
+    输出: 2
+     
+
+    限制：
+
+    1 <= 数组长度 <= 50000
+
+     
+
+    注意：本题与主站 169 题相同：https://leetcode-cn.com/problems/majority-element/
+
+
+
+    来源：力扣（LeetCode）
+    链接：https://leetcode-cn.com/problems/shu-zu-zhong-chu-xian-ci-shu-chao-guo-yi-ban-de-shu-zi-lcof
+    著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+    """
+    def majorityElement(self, nums: List[int]) -> int:
+        # Boyer-Moore
+        count = 0
+        candidate = 0
+        for num in nums:
+            if count==0:
+                candidate = num
+                count += 1
+            else:
+                count += (1 if candidate==num else -1)
+        return candidate
