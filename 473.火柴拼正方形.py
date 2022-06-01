28c0acd3e2c5f0466e2de8e5bca87d2900ea0463
BENULL <327137362@qq.com>
2022-06-01 21:51:19 +0800

    dfs

--- 473.火柴拼正方形.py
+++ 473.火柴拼正方形.py
@@ -48,32 +48,49 @@
 # @lc code=start
 class Solution:
     def makesquare(self, matchsticks: List[int]) -> bool:
-        if not matchsticks:
-            return False
+        # if not matchsticks:
+        #     return False
         
-        L = len(matchsticks)
+        # L = len(matchsticks)
 
-        perimeter = sum(matchsticks)
+        # perimeter = sum(matchsticks)
 
-        possible_side = perimeter // 4
-        if possible_side*4 != perimeter:
-            return False
+        # possible_side = perimeter // 4
+        # if possible_side*4 != perimeter:
+        #     return False
         
+        # matchsticks.sort(reverse=True)
+
+        # sums = [0 for _ in range(4)]
+
+        # def dfs(index):
+        #     if index == L:
+        #         return sums[0]==sums[1]==sums[2]==possible_side
+
+        #     for i in range(4):
+        #         if sums[i] + matchsticks[index] <= possible_side:
+        #             sums[i] += matchsticks[index]
+        #             if dfs(index+1):
+        #                 return True
+        #             sums[i] -= matchsticks[index]
+        #     return False
+        # return dfs(0)
+
+        _len = sum(matchsticks)
+        if _len % 4:
+            return False
         matchsticks.sort(reverse=True)
-
-        sums = [0 for _ in range(4)]
-
-        def dfs(index):
-            if index == L:
-                return sums[0]==sums[1]==sums[2]==possible_side
-
+        edges = [0]*4
+        def dfs(idx):
+            if idx == len(matchsticks):
+                return True
             for i in range(4):
-                if sums[i] + matchsticks[index] <= possible_side:
-                    sums[i] += matchsticks[index]
-                    if dfs(index+1):
-                        return True
-                    sums[i] -= matchsticks[index]
+                edges[i] += matchsticks[idx]
+                if edges[i]<= _len//4 and dfs(idx+1):
+                    return True
+                edges[i] -= matchsticks[idx]
             return False
+            
         return dfs(0)
 
 
